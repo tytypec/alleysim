c25a0e12431064526a0b955b0d773f28024d2f18
Unknown <typecora@gmail.com>
2017-07-26 15:44:39 -0500

    first commit

---
+++ GameManager.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static int appleScoreADJ;
+    public static int pnutScoreADJ;
+    public static int eggScoreADJ;
+    public static int milkScoreADJ;
+    public static float oddsMaker;
+    public float periodReset = 2f;
+    public Vector3 spawnValues;
+    //public Vector3 spawnValuesY;
+    public GameObject applePre;
+    public GameObject nutPre;
+    public GameObject milkPre;
+    public GameObject eggPre;
+
+    private int allergicValue;
+    private int allergicValueA;
+    private int allergicValueB;
+    private int allergicValueC;
+    private float nextActionTime = 0.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        oddsMaker = Random.value;
+        //Debug.Log("oddsMarker" + oddsMaker);
+
+        AllergyOdds();
+        AllergyAssignment();
+
+
+
+    }
+
+
+
+    public void AllergyAssignment()
+    {
+        appleScoreADJ = allergicValue * (int)Mathf.Round((25 * (Random.value)));
+
+        pnutScoreADJ = allergicValueA * (int)Mathf.Round(50 * (Random.value));
+
+        eggScoreADJ = allergicValueB * (int)Mathf.Round(30 * (Random.value));
+
+        milkScoreADJ = allergicValueC * (int)Mathf.Round(30 * (Random.value));
+    }
+
+    private void AllergyOdds()
+    {
+
+        oddsMaker = Random.value;
+        Debug.Log("oddsMarker" + oddsMaker);
+
+        // apple and pnut allergy
+        if (oddsMaker < .3f)
+        {
+            allergicValue = -1;
+            allergicValueA = 1;
+        }
+
+        else
+        {
+            allergicValue = 1;
+            allergicValueA = 1;
+        }
+
+        //Egg Allergy
+        if (oddsMaker < .5f && oddsMaker > .9f)
+            allergicValueB = 1;
+
+        else
+            allergicValueB = -1;
+
+        //Milk Allergy
+        if (oddsMaker < .8f)
+            allergicValueC = -1;
+
+        else
+            allergicValueC = 1;
+
+        //if (oddsMaker > .3f)
+        //allergicValue = 1;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += periodReset;
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), Random.Range(-spawnValues.y, spawnValues.y), 1);
+            //Vector3 spawnPosition = new Vector3(1f, 1f, 1f);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(applePre, (spawnPosition), spawnRotation);
+
+            Vector3 spawnPositionA = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), Random.Range(-spawnValues.y, spawnValues.y), 1);
+            Instantiate(nutPre, (spawnPositionA), spawnRotation);
+
+            Vector3 spawnPositionB = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), Random.Range(-spawnValues.y, spawnValues.y), 1);
+            Instantiate(eggPre, (spawnPositionB), spawnRotation);
+
+            Vector3 spawnPositionC = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), Random.Range(-spawnValues.y, spawnValues.y), 1);
+            Instantiate(milkPre, (spawnPositionC), spawnRotation);
+
+
+            //Instantiate(nutPre, (spawnPosition), spawnRotation);
+            //Instantiate(eggPre, (spawnPosition), spawnRotation);
+            //Instantiate(milkPre, (spawnPosition), spawnRotation);
+            //Debug.Log("allergicValue" + allergicValue);
+        }
+
+
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,275 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;	//Allows us to use UI.
+using UnityEngine.SceneManagement;
+
+
+public class Player : MonoBehaviour
+{
+
+    public float Speed;
+    public Animator playerAnimator;
+    public Slider HP;
+    public Color FullHealthColor = Color.green;       // The color the health bar will be when on full health.
+    public Color ZeroHealthColor = Color.red;
+    public Color MidHealthColor = Color.yellow;
+    public int StartingHP = 120;
+    public Image FillImage;
+    public int HPDrainSpeed;
+    public Text yumYuck;
+    public float periodReset = 1.5f;
+    // public Color yum = Color.green;
+    // public Color yuck;
+    // public Color meh;
+
+
+
+    private Rigidbody2D rb;
+    private int hitPoints;
+    private bool Dead;
+    private int appleScore;
+    private int PnutScore;
+    private float nextActionTime = 0.0f;
+    // private int foodEaten;
+    //private int InTummy = 1;
+    //private float nextFire;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+
+        StartCoroutine(Hunger());
+
+        // foodEaten = InTummy;
+
+
+
+
+
+        hitPoints = StartingHP;
+        Dead = false;
+
+        // Update the health slider's value and color.
+        SetHealthUI();
+        AllergyAssign();
+
+
+
+
+    }
+
+    IEnumerator Hunger()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            hitPoints = hitPoints - HPDrainSpeed;
+            Debug.Log(hitPoints);
+            SetHealthUI();
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(-moveHorizontal, moveVertical);
+        rb.velocity = movement * Speed;
+        SetHealthUI();
+
+
+
+
+        if ((Mathf.Abs(Input.GetAxisRaw("Horizontal")) > .1) || (Mathf.Abs(Input.GetAxisRaw("Vertical")) > .1))
+        {
+            playerAnimator.Play("PlayerWalk");
+
+
+        }
+
+        else
+        {
+            playerAnimator.Play("PlayerBlink");
+        }
+
+
+
+
+        /*if (hitPoints <= 0f && !Dead)
+        {
+            OnDeath();
+        }*/
+    }
+
+    public void AllergyAssign()
+    {
+        //GameManager.appleScore;
+        //appleScore = GameManager.appleScoreADJ;
+        //PnutScore = -10;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+
+
+        if (other.tag == "Apple")
+        {
+
+
+            hitPoints += GameManager.appleScoreADJ;
+            // hitPoints += appleScore;
+
+            SetHealthUI();
+            DidILikeThatFood();
+
+            // SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
+
+
+            other.gameObject.SetActive(false);
+            Debug.Log("Apple value of" + GameManager.appleScoreADJ);
+
+
+        }
+
+        if (other.tag == "Pnut")
+        {
+
+
+
+            hitPoints += GameManager.pnutScoreADJ;
+
+            SetHealthUI();
+            DidILikeThatFood();
+
+            // SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
+
+
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.tag == "Egg")
+        {
+
+
+
+            hitPoints += GameManager.eggScoreADJ;
+
+            SetHealthUI();
+            DidILikeThatFood();
+
+            // SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
+
+
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.tag == "Milk")
+        {
+
+
+
+            hitPoints += GameManager.milkScoreADJ;
+
+            SetHealthUI();
+            DidILikeThatFood();
+
+            // SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
+
+
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    private void SetHealthUI()
+    {
+        // Set the slider's value appropriately.
+        HP.value = hitPoints;
+
+        // Interpolate the color of the bar between the choosen colours based on the current percentage of the starting health.
+        //FillImage.color = Color.Lerp(ZeroHealthColor, FullHealthColor, hitPoints / StartingHP);
+
+        if (hitPoints > 90f)
+            FillImage.color = Color.green;
+
+        if (hitPoints > 40f && hitPoints < 90f)
+            FillImage.color = Color.yellow;
+
+        if (hitPoints < 40f)
+            FillImage.color = Color.red;
+        if (hitPoints < 0f)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        Player.gameObject.SetActive(false);
+    }
+
+    public void DidILikeThatFood() { }
+    /*{
+
+        if (GameManager.appleScoreADJ >= 3 || GameManager.pnutScoreADJ >= 3 || GameManager.eggScoreADJ >= 3 || GameManager.milkScoreADJ >= 3)
+        {
+            yumYuck.color = Color.green;
+            yumYuck.text = "Yummy";
+
+        }
+
+        if (GameManager.appleScoreADJ > 15 || GameManager.pnutScoreADJ > 15 || GameManager.eggScoreADJ > 15 || GameManager.milkScoreADJ > 15)
+        {
+            yumYuck.color = Color.green;
+            yumYuck.text = "DELISH!";
+
+        }
+
+        if ((GameManager.appleScoreADJ < 3 && GameManager.appleScoreADJ > -3) || (GameManager.pnutScoreADJ < 3 && GameManager.pnutScoreADJ > -3) || (GameManager.eggScoreADJ < 3 && GameManager.eggScoreADJ > -3) || (GameManager.milkScoreADJ < 3 && GameManager.milkScoreADJ > -3))
+        {
+            yumYuck.color = Color.yellow;
+            yumYuck.text = "meh";
+        }
+
+        if (GameManager.appleScoreADJ < -3 || GameManager.pnutScoreADJ < -3 || GameManager.eggScoreADJ < -3 || GameManager.milkScoreADJ < -3)
+        {
+            yumYuck.color = Color.red;
+            yumYuck.text = "ew";
+        }
+
+        if (GameManager.appleScoreADJ < -15 || GameManager.pnutScoreADJ < -15 || GameManager.eggScoreADJ < -15 || GameManager.milkScoreADJ < -15)
+        {
+            yumYuck.color = Color.red;
+            yumYuck.text = "yuck!";
+        }
+
+        if (GameManager.appleScoreADJ < -30 || GameManager.pnutScoreADJ < -30 || GameManager.eggScoreADJ < -30 || GameManager.milkScoreADJ < -30)
+        {
+            yumYuck.color = Color.red;
+            yumYuck.text = "BARFFFF!";
+        }
+
+        else
+            yumYuck.text = "";
+    }
+
+
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += periodReset;
+
+            yumYuck.color = Color.black;
+            yumYuck.text = "";
+
+        }
+    }*/
+}
